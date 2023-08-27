8ad9f8ec77aacfd11921e240321f9fbd2c251a13
Ruma303 <matteoh8@hotmail.it>
2023-08-27 15:41:29 +0200

    useTransition ok

--- src/useTransition/UseTransition.jsx
+++ src/useTransition/UseTransition.jsx
@@ -1,47 +1,43 @@
 import React, { useEffect, useState, useTransition } from 'react';
 
 function UseTransition() {
-  const data = [];
-  for (let i = 1; i <= 1000; i++) {
-    data.push(i);
-  }
-  const [input, setInput] = useState("");
-  const [isPending, startTransition] = useTransition();
-  const [filteredData, setFilteredData] = useState(data);
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    startTransition(() => {
-      setInput(value);
-    });
-};
-
-useEffect(() => {
-    if (!input) {
-        setFilteredData(data);
+    const data = [];
+    for (let i = 1; i <= 10000; i++) {
+        data.push(i);
     }
-    setFilteredData(data.filter((e) => e.toString().includes(input)));
-  }, [input]);
+    const [input, setInput] = useState("");
+    const [isPending, startTransition] = useTransition();
+    const [filteredData, setFilteredData] = useState(data);
 
-  return (
-    <div>
-      <h1>useTransition</h1>
-      <input type="text" onChange={handleChange} />
-      {!isPending ? (
+    const handleChange = (e) => {
+        const inputValue = e.target.value;
+        //* Questo update non è così importante.
+        startTransition(() => {
+            setInput(inputValue);
+        });
+    };
+
+    useEffect(() => {
+        if (!input) {
+            setFilteredData(data);
+        }
+        setFilteredData(data.filter((e) => e.toString().includes(input)));
+    }, [input]);
+
+    return (
         <div>
-          {filteredData.map((item, index) => (
-            <p key={index}>{item}</p>
-          ))}
-        </div>
-      ) : (
-        <div>
-          {data.map((item, index) => (
-            <p key={index}>{item}</p>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+            <h1>useTransition</h1>
+            <input type="text" onChange={handleChange} />
+            {isPending ? ( //? Se i dati non sono stati calcolati, isPending === true
+                <h3>Loading...</h3>
+            ) : ( //? Quando i dati saranno stati calcolati, verranno mostrati
+                <div>
+                    {filteredData.map((item, index) => (
+                        <p key={index}>{item}</p>
+                    ))}
+                </div>
+            )}
+        </div>);
 }
 
 export default UseTransition;
